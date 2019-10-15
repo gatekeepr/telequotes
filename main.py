66547af12d4005cfb31d2746aee78adc99e6bcda
gate <gate@black-mesa.xyz>
2019-10-15 11:54:49 +0200

    you can now specify a keyword for desired quotes

--- main.py
+++ main.py
@@ -4,31 +4,35 @@ import logging, csv
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
 token = open("config.txt","r").read().strip()
 updater = Updater(token=token, use_context=True)
 dispatcher = updater.dispatcher
 
+
+#counts all lines in the quotefile
 def countquotes(filename):
     with open(filename) as f:
         return sum(1 for line in f)
 
+#adds a quote to the database
 def add(update, context):
-
-    print(update.message.reply_to_message)
 
     text = update.message.reply_to_message.text
     date = str(update.message.reply_to_message.date.now())
-    user = update.message.reply_to_message.from_user.username
+    user = update.message.reply_to_message.from_user.first_name
 
-    print(text)
-    print(date)
+    #debug output
     print(user)
+    print(date)
+    print(text)
+
+
 
     with open('quotes.csv', 'a', encoding='utf-8') as csvfile:
         fieldnames = ['id', 'username', 'date', 'quote']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         counter = countquotes('quotes.csv')
+
         writer.writerow({
             'id' : counter,
             'username' : user,
@@ -36,24 +40,45 @@ def add(update, context):
             'quote' : text
         })
 
+
+#sends a fully random quote if no keyword is passed
 def random(update, context):
 
+    mode = 1
     counter = countquotes('quotes.csv')
     pseudorandom = randrange(counter-1)
+
+    #keyword specified in message, save it
+    if(len(update.message.text.partition(' ')[2]) > 2):
+        keyword = update.message.text.partition(' ')[2]
+        mode = 2
 
     with open('quotes.csv', 'r', encoding='utf-8') as csvfile:
         fieldnames = ['id', 'username', 'date', 'quote']
         reader = csv.DictReader(csvfile, delimiter= ',')
-        text="NULL"
-        date="NEVER"
-        user="ANON"
+        candidates = []
 
-        for row in reader:
-            if(reader.line_num - 2 == pseudorandom):
-                 text = row['quote']
-                 date = row['date']
-                 user = row['username']
+        #keyword specified
+        if(mode == 2):
+            for row in reader:
+                quote = row['quote']
+                if keyword in quote:
+                    candidates.append(row)
+            counter = len(candidates)
+            pseudorandom = randrange(counter)
+            for row in candidates:
+                if(candidates.index(row) == pseudorandom):
+                    text = row['quote']
+                    date = row['date']
+                    user = row['username']
 
+        #no keyword
+        else:
+            for row in reader:
+                if(reader.line_num - 2 == pseudorandom):
+                    text = row['quote']
+                    date = row['date']
+                    user = row['username']
 
     finalstring = user + " @ " + date + ": \n" + text
     context.bot.send_message(chat_id=update.message.chat_id, text=finalstring)
